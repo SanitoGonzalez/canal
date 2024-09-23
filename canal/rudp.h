6bb4b661c4f4e3e6660009968c6ed158f55c726f
Sanito Gonzalez <ddalkisoup23@gmail.com>
2024-09-23 17:54:43 +0900

    Add rudp.h

---
+++ canal/rudp.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <stdint.h>
+
+#define CANAL_VERSION 1
+
+/*
+* SYN: Initiate an RUDP connection between two hosts.
+* ACK: Reply the correct sequential message.
+* EAK: Reply the incorrect or unsequenced message.
+* RST: Reset a RUDP connection
+* NUL: Keep connection between two hosts.
+* (5): Not used yet.
+* (6~7): VER NO. Used to indicated the version of RUDP 
+*/
+enum CanalHeaderControlFlag {
+    CANAL_HEADER_CONTROL_SYN = 1 << 0,
+    CANAL_HEADER_CONTROL_ACK = 1 << 1,
+    CANAL_HEADER_CONTROL_EAK = 1 << 2,
+    CANAL_HEADER_CONTROL_RST = 1 << 3,
+    CANAL_HEADER_CONTROL_NUL = 1 << 4,
+};
+
+enum CanalPacketFlag {
+    CANAL_PACKET_ORDERED =  1 << 0,
+    CANAL_PACKET_UNORDERED = 1 << 1,
+    CANAL_PACKET_RELIABLE = 1 << 2,
+    CANAL_PACKET_UNRELIABLE = 1 << 3,
+};
+
+struct CanalHeader {
+    uint8_t control;
+    uint8_t header_length;
+    uint16_t sequence;
+    uint16_t checksum;
+    uint16_t custom;
+};
+
+static void canal_write_header_control(struct CanalHeader* header, CanalHeaderControlFlag control) {
+    header->control = 0;
+    header->control |= (control & 0b0011'1111);
+    header->control |= (CANAL_VERSION & 0b0000'0011) << 6;
+}
--- canal/xdp.bpf.c
+++ canal/xdp.bpf.c
@@ -1,9 +1,39 @@
 #include <linux/bpf.h>
+#include <linux/if_ether.h>
+#include <linux/in.h>
+#include <linux/ip.h>
 #include <bpf/bpf_helpers.h>
+#include <canal/rudp.h>
 
-char LICENSE[] SEC("license") = "Dual MIT/GSL";
+struct bpf_map_def SEC("maps") acknowledged = {
+    .type = BPF_MAP_TYPE_PERF_EVENT_ARRAY,
+    .key_size = sizeof(int)
+    .value_size = sizeof(u32),
+    .max_entries = 2,
+};
 
-SEC("xdp")
-int xdp_prog(struct xdp_md* ctx) {
+SEC("ingress_controller")
+int ingress_controller_prog(struct xdp_md* ctx) {
+    int ipsize = 0;
+    void* data = (void*)(long)ctx->data;
+    void* data_end = (void*)(long)ctx->data_end;
+    struct ethhdr* eth = data;
+    struct iphdr* ip;
+
+    ipsize = sizeof(*eth);
+    ip = data + ipsize;
+    ipsize += sizeof(struct iphdr);
+
+    if (data + ipsize > data_end) return XDP_DROP;
+    if (ip->protocol != IPPROTO_UDP) return XDP_DROP;
+
+    // 1. Parse RUDP header
+
+    // 2-1. DROP if ACK and notify to egress controller and user program
+
+    // 2-2. Else, redirect?
+
     return XDP_PASS;
 }
+
+char LICENSE[] SEC("license") = "Dual MIT/GPL";
